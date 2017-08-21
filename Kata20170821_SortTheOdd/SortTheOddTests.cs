3898c5d773b4734f6c4015fcd08e12149a30500c
cash <cash@cashwu.com>
2017-08-21 21:04:04 +0800

    input empty array test case

--- Kata20170821_SortTheOdd/SortTheOddTests.cs
+++ Kata20170821_SortTheOdd/SortTheOddTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170821_SortTheOdd
@@ -7,8 +8,24 @@ namespace Kata20170821_SortTheOdd
     public class SortTheOddTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_empty_array_should_return_empty_array()
         {
+            SortOddArrayShouldBe(new int[] { }, new int[] { });
+        }
+
+        private static void SortOddArrayShouldBe(int[] expected, int[] array)
+        {
+            var kata = new Kata();
+            var actual = kata.SortArray(array);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int[] SortArray(int[] array)
+        {
+            return array;
         }
     }
 }
